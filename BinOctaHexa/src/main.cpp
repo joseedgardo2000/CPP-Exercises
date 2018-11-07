8c5a13dfac6466691f99cedfe931dc08b78d09e3
José Edgardo Morales Barroso <edgardo.morales@quetzalcloud.com>
2018-11-06 23:48:54 -0600

    BinOctaHexa - Init
    BinOctaHexa - Init

---
+++ BinOctaHexa/src/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 06/11/2018
+ *  Author: Jose Edgardo Morales Barroso
+ */
+
+#include <iostream>
+
+int main()
+{
+	int a = 10;
+	int b = 010;
+	int c = 0x10;
+	int d = 0b10;
+
+	std::cout << "decimal (10): " << a << std::endl;
+	std::cout << "octal (10): " << b << std::endl;
+	std::cout << "hexadecimal (10): " << c << std::endl;
+	std::cout << "binario (10): " << d << std::endl;
+}
+
+
