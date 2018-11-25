2ce01ad84e3801c778250878bbc16c165aa66d60
José Edgardo Morales Barroso <edgardo.morales@quetzalcloud.com>
2018-11-25 16:51:54 -0600

    VirtualFunctions - Init
    VirtualFunctions - Init

---
+++ VirtualFunctions/include/VirtualFunctions/Base.h
@@ -0,0 +1,31 @@
+/*
+ * Base.h
+ *
+ *  Created on: 25/11/2018
+ *      Author: José Edgardo Morales Barroso
+ */
+
+#ifndef INCLUDE_FUNCTIONOVERRIDING_BASE_H_
+#define INCLUDE_FUNCTIONOVERRIDING_BASE_H_
+
+#include <iostream>
+#include <string>
+
+class Base {
+private:
+	int x;
+	std::string name;
+public:
+	//----- CONSTRUCTORS/DESTRUCTORS ---------//
+	Base(int a = 0, std::string name = "");
+	//----- ACCESSORS/MUTATORS ---------------//
+	int getX() const;
+	void setX(int x);
+	const std::string& getName() const;
+	void setName(const std::string& name);
+	//----- FACILATORS -----------------------//
+	virtual void display();
+
+};
+
+#endif /* INCLUDE_FUNCTIONOVERRIDING_BASE_H_ */
